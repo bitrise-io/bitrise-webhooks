a977ffd1aa58205b9aee8190c8a6cd333792c236
Viktor Benei <viktor.benei@gmail.com>
2016-02-06 16:14:53 +0100

    GitHub : both CodePush and PullRequest are now handled & tested

--- providers/github/github.go
+++ providers/github/github.go
@@ -10,6 +10,7 @@ import (
 	"github.com/bitrise-io/bitrise-webhooks/bitriseapi"
 	"github.com/bitrise-io/bitrise-webhooks/providers"
 	"github.com/bitrise-io/go-utils/httputil"
+	"github.com/bitrise-io/go-utils/sliceutil"
 )
 
 // CommitModel ...
@@ -24,6 +25,28 @@ type CodePushEventModel struct {
 	Ref        string      `json:"ref"`
 	Deleted    bool        `json:"deleted"`
 	HeadCommit CommitModel `json:"head_commit"`
+}
+
+// BranchInfoModel ...
+type BranchInfoModel struct {
+	Ref        string `json:"ref"`
+	CommitHash string `json:"sha"`
+}
+
+// PullRequestInfoModel ...
+type PullRequestInfoModel struct {
+	BranchInfo BranchInfoModel `json:"head"`
+	Title      string          `json:"title"`
+	Body       string          `json:"body"`
+	Merged     bool            `json:"merged"`
+	Mergeable  *bool           `json:"mergeable"`
+}
+
+// PullRequestEventModel ...
+type PullRequestEventModel struct {
+	Action          string               `json:"action"`
+	PullRequestID   int                  `json:"number"`
+	PullRequestInfo PullRequestInfoModel `json:"pull_request"`
 }
 
 // HookProvider ...
@@ -55,13 +78,26 @@ func (hp HookProvider) HookCheck(header http.Header) providers.HookCheckModel {
 }
 
 func transformCodePushEvent(codePushEvent CodePushEventModel) providers.HookTransformResultModel {
+	if codePushEvent.Deleted {
+		return providers.HookTransformResultModel{
+			Error:      errors.New("This is a 'Deleted' event, no build can be started"),
+			ShouldSkip: true,
+		}
+	}
+
 	headCommit := codePushEvent.HeadCommit
 	if !headCommit.Distinct {
-		return providers.HookTransformResultModel{Error: errors.New("Head Commit is not Distinct"), ShouldSkip: true}
+		return providers.HookTransformResultModel{
+			Error:      errors.New("Head Commit is not Distinct"),
+			ShouldSkip: true,
+		}
 	}
 
 	if !strings.HasPrefix(codePushEvent.Ref, "refs/heads/") {
-		return providers.HookTransformResultModel{Error: fmt.Errorf("Ref (%s) is not a head ref", codePushEvent.Ref), ShouldSkip: true}
+		return providers.HookTransformResultModel{
+			Error:      fmt.Errorf("Ref (%s) is not a head ref", codePushEvent.Ref),
+			ShouldSkip: true,
+		}
 	}
 	branch := strings.TrimPrefix(codePushEvent.Ref, "refs/heads/")
 
@@ -74,11 +110,59 @@ func transformCodePushEvent(codePushEvent CodePushEventModel) providers.HookTran
 	}
 }
 
+func transformPullRequestEvent(pullRequest PullRequestEventModel) providers.HookTransformResultModel {
+	if pullRequest.Action == "" {
+		return providers.HookTransformResultModel{
+			Error:      errors.New("No Pull Request action specified"),
+			ShouldSkip: true,
+		}
+	}
+	if !sliceutil.IsStringInSlice(pullRequest.Action, []string{"opened", "reopened", "synchronize"}) {
+		return providers.HookTransformResultModel{
+			Error:      fmt.Errorf("Pull Request action doesn't require a build: %s", pullRequest.Action),
+			ShouldSkip: true,
+		}
+	}
+	if pullRequest.PullRequestInfo.Merged {
+		return providers.HookTransformResultModel{
+			Error:      errors.New("Pull Request already merged"),
+			ShouldSkip: true,
+		}
+	}
+	if pullRequest.PullRequestInfo.Mergeable != nil && *pullRequest.PullRequestInfo.Mergeable == false {
+		return providers.HookTransformResultModel{
+			Error:      errors.New("Pull Request is not mergeable"),
+			ShouldSkip: true,
+		}
+	}
+
+	commitMsg := pullRequest.PullRequestInfo.Title
+	if pullRequest.PullRequestInfo.Body != "" {
+		commitMsg = fmt.Sprintf("%s\n\n%s", commitMsg, pullRequest.PullRequestInfo.Body)
+	}
+
+	return providers.HookTransformResultModel{
+		TriggerAPIParams: bitriseapi.TriggerAPIParamsModel{
+			CommitHash:    pullRequest.PullRequestInfo.BranchInfo.CommitHash,
+			CommitMessage: commitMsg,
+			Branch:        pullRequest.PullRequestInfo.BranchInfo.Ref,
+			PullRequestID: &pullRequest.PullRequestID,
+		},
+	}
+}
+
 // Transform ...
 func (hp HookProvider) Transform(r *http.Request) providers.HookTransformResultModel {
 	if r.Body == nil {
 		return providers.HookTransformResultModel{
 			Error: fmt.Errorf("Failed to read content of request body: no or empty request body"),
+		}
+	}
+
+	contentType, err := httputil.GetSingleValueFromHeader("Content-Type", r.Header)
+	if err != nil {
+		return providers.HookTransformResultModel{
+			Error: fmt.Errorf("Failed to get Content-Type from Header"),
 		}
 	}
 
@@ -91,13 +175,6 @@ func (hp HookProvider) Transform(r *http.Request) providers.HookTransformResultM
 
 	if ghEvent == "push" {
 		// code push
-		contentType, err := httputil.GetSingleValueFromHeader("Content-Type", r.Header)
-		if err != nil {
-			return providers.HookTransformResultModel{
-				Error: fmt.Errorf("Failed to get Content-Type from Header"),
-			}
-		}
-
 		var codePushEvent CodePushEventModel
 		if contentType == "application/json" {
 			if err := json.NewDecoder(r.Body).Decode(&codePushEvent); err != nil {
@@ -117,7 +194,30 @@ func (hp HookProvider) Transform(r *http.Request) providers.HookTransformResultM
 			}
 		}
 		return transformCodePushEvent(codePushEvent)
+
+	} else if ghEvent == "pull_request" {
+		var pullRequestEvent PullRequestEventModel
+		if contentType == "application/json" {
+			if err := json.NewDecoder(r.Body).Decode(&pullRequestEvent); err != nil {
+				return providers.HookTransformResultModel{Error: fmt.Errorf("Failed to parse request body: %s", err)}
+			}
+		} else if contentType == "application/x-www-form-urlencoded" {
+			payloadValue := r.PostFormValue("payload")
+			if payloadValue == "" {
+				return providers.HookTransformResultModel{Error: fmt.Errorf("Failed to parse request body: empty payload")}
+			}
+			if err := json.NewDecoder(strings.NewReader(payloadValue)).Decode(&pullRequestEvent); err != nil {
+				return providers.HookTransformResultModel{Error: fmt.Errorf("Failed to parse payload: %s", err)}
+			}
+		} else {
+			return providers.HookTransformResultModel{
+				Error: fmt.Errorf("Unsupported Content-Type: %s", contentType),
+			}
+		}
+		return transformPullRequestEvent(pullRequestEvent)
 	}
 
-	return providers.HookTransformResultModel{}
+	return providers.HookTransformResultModel{
+		Error: fmt.Errorf("Unsupported GitHub event type: %s", ghEvent),
+	}
 }
--- providers/github/github_test.go
+++ providers/github/github_test.go
@@ -7,7 +7,35 @@ import (
 	"testing"
 
 	"github.com/bitrise-io/bitrise-webhooks/bitriseapi"
+	"github.com/bitrise-io/go-utils/pointers"
 	"github.com/stretchr/testify/require"
+)
+
+const (
+	sampleCodePushData = `{
+  "ref": "refs/heads/master",
+  "deleted": false,
+  "head_commit": {
+    "distinct": true,
+    "id": "83b86e5f286f546dc5a4a58db66ceef44460c85e",
+    "message": "re-structuring Hook Providers, with added tests"
+  }
+}`
+
+	samplePullRequestData = `{
+  "action": "opened",
+  "number": 12,
+  "pull_request": {
+    "head": {
+      "ref": "master",
+      "sha": "83b86e5f286f546dc5a4a58db66ceef44460c85e"
+    },
+    "title": "PR test",
+    "body": "PR text body",
+    "merged": false,
+    "mergeable": true
+  }
+}`
 )
 
 func Test_HookProvider_HookCheck(t *testing.T) {
@@ -70,7 +98,7 @@ func Test_HookProvider_HookCheck(t *testing.T) {
 func Test_HookProvider_Transform(t *testing.T) {
 	provider := HookProvider{}
 
-	t.Log("Code Push")
+	t.Log("Code Push - should not be skipped")
 	{
 		request := http.Request{
 			Header: http.Header{"X-Github-Event": {"push"}},
@@ -80,7 +108,7 @@ func Test_HookProvider_Transform(t *testing.T) {
 		require.False(t, hookTransformResult.ShouldSkip)
 	}
 
-	t.Log("Pull Request")
+	t.Log("Pull Request - should not be skipped")
 	{
 		request := http.Request{
 			Header: http.Header{"X-Github-Event": {"pull_request"}},
@@ -99,6 +127,45 @@ func Test_HookProvider_Transform(t *testing.T) {
 		require.False(t, hookTransformResult.ShouldSkip)
 		require.EqualError(t, hookTransformResult.Error, "Failed to read content of request body: no or empty request body")
 	}
+
+	t.Log("Code Push - should be handled")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"X-Github-Event": {"push"},
+				"Content-Type":   {"application/json"},
+			},
+			Body: ioutil.NopCloser(strings.NewReader(sampleCodePushData)),
+		}
+		hookTransformResult := provider.Transform(&request)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, bitriseapi.TriggerAPIParamsModel{
+			CommitHash:    "83b86e5f286f546dc5a4a58db66ceef44460c85e",
+			CommitMessage: "re-structuring Hook Providers, with added tests",
+			Branch:        "master",
+		}, hookTransformResult.TriggerAPIParams)
+	}
+
+	t.Log("Pull Request - should be handled")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"X-Github-Event": {"pull_request"},
+				"Content-Type":   {"application/json"},
+			},
+			Body: ioutil.NopCloser(strings.NewReader(samplePullRequestData)),
+		}
+		hookTransformResult := provider.Transform(&request)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, bitriseapi.TriggerAPIParamsModel{
+			CommitHash:    "83b86e5f286f546dc5a4a58db66ceef44460c85e",
+			CommitMessage: "PR test\n\nPR text body",
+			Branch:        "master",
+			PullRequestID: pointers.NewIntPtr(12),
+		}, hookTransformResult.TriggerAPIParams)
+	}
 }
 
 func Test_transformCodePushEvent(t *testing.T) {
@@ -110,6 +177,19 @@ func Test_transformCodePushEvent(t *testing.T) {
 		hookTransformResult := transformCodePushEvent(codePush)
 		require.True(t, hookTransformResult.ShouldSkip)
 		require.EqualError(t, hookTransformResult.Error, "Head Commit is not Distinct")
+	}
+
+	t.Log("This is a 'deleted' event")
+	{
+		codePush := CodePushEventModel{
+			HeadCommit: CommitModel{
+				Distinct: true,
+			},
+			Deleted: true,
+		}
+		hookTransformResult := transformCodePushEvent(codePush)
+		require.True(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "This is a 'Deleted' event, no build can be started")
 	}
 
 	t.Log("Not a head ref")
@@ -135,11 +215,137 @@ func Test_transformCodePushEvent(t *testing.T) {
 		}
 		hookTransformResult := transformCodePushEvent(codePush)
 		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
 		require.Equal(t, bitriseapi.TriggerAPIParamsModel{
 			CommitHash:    "83b86e5f286f546dc5a4a58db66ceef44460c85e",
 			CommitMessage: "re-structuring Hook Providers, with added tests",
 			Branch:        "master",
 		}, hookTransformResult.TriggerAPIParams)
+	}
+}
+
+func Test_transformPullRequestEvent(t *testing.T) {
+	t.Log("Unsupported Pull Request action")
+	{
+		pullRequest := PullRequestEventModel{
+			Action: "labeled",
+		}
+		hookTransformResult := transformPullRequestEvent(pullRequest)
+		require.True(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "Pull Request action doesn't require a build: labeled")
+	}
+
+	t.Log("Empty Pull Request action")
+	{
+		pullRequest := PullRequestEventModel{}
+		hookTransformResult := transformPullRequestEvent(pullRequest)
+		require.True(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "No Pull Request action specified")
+	}
+
+	t.Log("Already Merged")
+	{
+		pullRequest := PullRequestEventModel{
+			Action: "opened",
+			PullRequestInfo: PullRequestInfoModel{
+				Merged: true,
+			},
+		}
+		hookTransformResult := transformPullRequestEvent(pullRequest)
+		require.True(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "Pull Request already merged")
+	}
+
+	t.Log("Not Mergeable")
+	{
+		pullRequest := PullRequestEventModel{
+			Action: "reopened",
+			PullRequestInfo: PullRequestInfoModel{
+				Merged:    false,
+				Mergeable: pointers.NewBoolPtr(false),
+			},
+		}
+		hookTransformResult := transformPullRequestEvent(pullRequest)
+		require.True(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "Pull Request is not mergeable")
+	}
+
+	t.Log("Mergeable: not yet decided")
+	{
+		pullRequest := PullRequestEventModel{
+			Action:        "synchronize",
+			PullRequestID: 12,
+			PullRequestInfo: PullRequestInfoModel{
+				Title:     "PR test",
+				Merged:    false,
+				Mergeable: nil,
+				BranchInfo: BranchInfoModel{
+					Ref:        "master",
+					CommitHash: "83b86e5f286f546dc5a4a58db66ceef44460c85e",
+				},
+			},
+		}
+		hookTransformResult := transformPullRequestEvent(pullRequest)
 		require.False(t, hookTransformResult.ShouldSkip)
+		require.NoError(t, hookTransformResult.Error)
+		require.Equal(t, bitriseapi.TriggerAPIParamsModel{
+			CommitHash:    "83b86e5f286f546dc5a4a58db66ceef44460c85e",
+			CommitMessage: "PR test",
+			Branch:        "master",
+			PullRequestID: pointers.NewIntPtr(12),
+		}, hookTransformResult.TriggerAPIParams)
+	}
+
+	t.Log("Mergeable: true")
+	{
+		pullRequest := PullRequestEventModel{
+			Action:        "synchronize",
+			PullRequestID: 12,
+			PullRequestInfo: PullRequestInfoModel{
+				Title:     "PR test",
+				Merged:    false,
+				Mergeable: pointers.NewBoolPtr(true),
+				BranchInfo: BranchInfoModel{
+					Ref:        "master",
+					CommitHash: "83b86e5f286f546dc5a4a58db66ceef44460c85e",
+				},
+			},
+		}
+		hookTransformResult := transformPullRequestEvent(pullRequest)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, bitriseapi.TriggerAPIParamsModel{
+			CommitHash:    "83b86e5f286f546dc5a4a58db66ceef44460c85e",
+			CommitMessage: "PR test",
+			Branch:        "master",
+			PullRequestID: pointers.NewIntPtr(12),
+		}, hookTransformResult.TriggerAPIParams)
+	}
+
+	t.Log("Pull Request - Title & Body")
+	{
+		pullRequest := PullRequestEventModel{
+			Action:        "opened",
+			PullRequestID: 12,
+			PullRequestInfo: PullRequestInfoModel{
+				Title:     "PR test",
+				Body:      "PR text body",
+				Merged:    false,
+				Mergeable: pointers.NewBoolPtr(true),
+				BranchInfo: BranchInfoModel{
+					Ref:        "master",
+					CommitHash: "83b86e5f286f546dc5a4a58db66ceef44460c85e",
+				},
+			},
+		}
+		hookTransformResult := transformPullRequestEvent(pullRequest)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, bitriseapi.TriggerAPIParamsModel{
+			CommitHash:    "83b86e5f286f546dc5a4a58db66ceef44460c85e",
+			CommitMessage: "PR test\n\nPR text body",
+			Branch:        "master",
+			PullRequestID: pointers.NewIntPtr(12),
+		}, hookTransformResult.TriggerAPIParams)
 	}
 }
