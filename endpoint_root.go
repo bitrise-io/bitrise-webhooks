06b6574c98b8e7c7bd28280342183d4b9adf0835
Viktor Benei <viktor.benei@gmail.com>
2016-02-06 16:14:26 +0100

    root endpoint : now includes the Environment Mode

--- endpoint_root.go
+++ endpoint_root.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/bitrise-io/bitrise-webhooks/config"
 )
 
 // RootRespModel ...
 type RootRespModel struct {
-	Message string `json:"message"`
-	Version string `json:"version"`
-	Time    string `json:"time"`
+	Message         string `json:"message"`
+	Version         string `json:"version"`
+	Time            string `json:"time"`
+	EnvironmentMode string `json:"environment_mode"`
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	resp := RootRespModel{
-		Message: "Welcome to bitrise-webhooks!",
-		Version: VERSION,
-		Time:    fmt.Sprintf("%s", time.Now()),
+		Message:         "Welcome to bitrise-webhooks!",
+		Version:         VERSION,
+		Time:            fmt.Sprintf("%s", time.Now()),
+		EnvironmentMode: config.GetServerEnvMode(),
 	}
 
 	respondWithSuccess(w, resp)
