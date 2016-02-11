ca6a7a8350493e2debe063bfa4034f70f55b1bd4
Viktor Benei <viktor.benei@gmail.com>
2016-02-11 15:08:41 +0100

    bitbucket-v2 is now a supported provider

--- service/hook/endpoint.go
+++ service/hook/endpoint.go
@@ -11,6 +11,7 @@ import (
 	"github.com/bitrise-io/bitrise-webhooks/config"
 	"github.com/bitrise-io/bitrise-webhooks/metrics"
 	"github.com/bitrise-io/bitrise-webhooks/service"
+	"github.com/bitrise-io/bitrise-webhooks/service/hook/bitbucketv2"
 	hookCommon "github.com/bitrise-io/bitrise-webhooks/service/hook/common"
 	"github.com/bitrise-io/bitrise-webhooks/service/hook/github"
 	"github.com/gorilla/mux"
@@ -18,8 +19,8 @@ import (
 
 func supportedProviders() map[string]hookCommon.Provider {
 	return map[string]hookCommon.Provider{
-		"github": github.HookProvider{},
-		// "bitbucket-v2": bitbucketv2.HookProvider{},
+		"github":       github.HookProvider{},
+		"bitbucket-v2": bitbucketv2.HookProvider{},
 	}
 }
 
