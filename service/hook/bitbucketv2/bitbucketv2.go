06b45110e2563ae0dbd21ad945ca4bd8c271d5e0
Viktor Benei <viktor.benei@gmail.com>
2016-02-11 15:03:51 +0100

    BitbucketV2 implementation - DONE

--- service/hook/bitbucketv2/bitbucketv2.go
+++ service/hook/bitbucketv2/bitbucketv2.go
@@ -1,11 +1,52 @@
 package bitbucketv2
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/bitrise-io/bitrise-webhooks/bitriseapi"
+	hookCommon "github.com/bitrise-io/bitrise-webhooks/service/hook/common"
 	"github.com/bitrise-io/go-utils/httputil"
 )
+
+// --------------------------
+// --- Webhook Data Model ---
+
+// ChangeItemTargetModel ...
+type ChangeItemTargetModel struct {
+	Type          string `json:"type"`
+	CommitHash    string `json:"hash"`
+	CommitMessage string `json:"message"`
+}
+
+// ChangeItemModel ...
+type ChangeItemModel struct {
+	Type   string                `json:"type"`
+	Name   string                `json:"name"`
+	Target ChangeItemTargetModel `json:"target"`
+}
+
+// ChangeInfoModel ...
+type ChangeInfoModel struct {
+	ChangeNewItem ChangeItemModel `json:"new"`
+}
+
+// PushInfoModel ...
+type PushInfoModel struct {
+	Changes []ChangeInfoModel `json:"changes"`
+}
+
+// CodePushEventModel ...
+type CodePushEventModel struct {
+	PushInfo PushInfoModel `json:"push"`
+}
+
+// ---------------------------------------
+// --- Webhook Provider Implementation ---
+
+// HookProvider ...
+type HookProvider struct{}
 
 func detectContentTypeUserAgentAndEventKey(header http.Header) (string, string, string, error) {
 	contentType, err := httputil.GetSingleValueFromHeader("Content-Type", header)
@@ -24,4 +65,82 @@ func detectContentTypeUserAgentAndEventKey(header http.Header) (string, string, 
 	}
 
 	return contentType, userAgent, eventKey, nil
+}
+
+func transformCodePushEvent(codePushEvent CodePushEventModel) hookCommon.TransformResultModel {
+	if len(codePushEvent.PushInfo.Changes) < 1 {
+		return hookCommon.TransformResultModel{
+			Error: fmt.Errorf("No 'changes' included in the webhook, can't start a build."),
+		}
+	}
+
+	triggerAPIParams := []bitriseapi.TriggerAPIParamsModel{}
+	errs := []string{}
+	for _, aChnage := range codePushEvent.PushInfo.Changes {
+		aNewItm := aChnage.ChangeNewItem
+		if aNewItm.Type != "branch" {
+			errs = append(errs, fmt.Sprintf("Not a type=branch change. Type was: %s", aNewItm.Type))
+			continue
+		}
+		if aNewItm.Target.Type != "commit" {
+			errs = append(errs, fmt.Sprintf("Target: Not a type=commit change. Type was: %s", aNewItm.Target.Type))
+			continue
+		}
+
+		aTriggerAPIParams := bitriseapi.TriggerAPIParamsModel{
+			CommitHash:    aNewItm.Target.CommitHash,
+			CommitMessage: aNewItm.Target.CommitMessage,
+			Branch:        aNewItm.Name,
+		}
+		triggerAPIParams = append(triggerAPIParams, aTriggerAPIParams)
+	}
+	if len(triggerAPIParams) < 1 {
+		return hookCommon.TransformResultModel{
+			Error: fmt.Errorf("'changes' specified in the webhook, but none can be transformed into a build. Collected errors: %s", errs),
+		}
+	}
+
+	return hookCommon.TransformResultModel{
+		TriggerAPIParams: triggerAPIParams,
+	}
+}
+
+// Transform ...
+func (hp HookProvider) Transform(r *http.Request) hookCommon.TransformResultModel {
+	contentType, userAgent, eventKey, err := detectContentTypeUserAgentAndEventKey(r.Header)
+	if err != nil {
+		return hookCommon.TransformResultModel{
+			Error: fmt.Errorf("Issue with Headers: %s", err),
+		}
+	}
+	if contentType != "application/json" {
+		return hookCommon.TransformResultModel{
+			Error: fmt.Errorf("Content-Type is not supported: %s", contentType),
+		}
+	}
+	if userAgent != "Bitbucket-Webhooks/2.0" {
+		return hookCommon.TransformResultModel{
+			Error: fmt.Errorf("User-Agent is not supported: %s", userAgent),
+		}
+	}
+	if eventKey != "repo:push" {
+		return hookCommon.TransformResultModel{
+			Error: fmt.Errorf("X-Event-Key is not supported: %s", eventKey),
+		}
+	}
+
+	if r.Body == nil {
+		return hookCommon.TransformResultModel{
+			Error: fmt.Errorf("Failed to read content of request body: no or empty request body"),
+		}
+	}
+
+	var codePushEvent CodePushEventModel
+	if err := json.NewDecoder(r.Body).Decode(&codePushEvent); err != nil {
+		return hookCommon.TransformResultModel{
+			Error: fmt.Errorf("Failed to parse request body as JSON: %s", err),
+		}
+	}
+
+	return transformCodePushEvent(codePushEvent)
 }
--- service/hook/bitbucketv2/bitbucketv2_test.go
+++ service/hook/bitbucketv2/bitbucketv2_test.go
@@ -1,10 +1,44 @@
 package bitbucketv2
 
 import (
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 
+	"github.com/bitrise-io/bitrise-webhooks/bitriseapi"
 	"github.com/stretchr/testify/require"
+)
+
+const (
+	sampleCodePushData = `{
+"push": {
+	"changes": [
+		{
+			"new": {
+				"name": "master",
+				"type": "branch",
+				"target": {
+					"type": "commit",
+					"message": "auto-test",
+					"hash": "966d0bfe79b80f97268c2f6bb45e65e79ef09b31"
+				}
+			}
+		},
+		{
+			"new": {
+				"name": "test",
+				"type": "branch",
+				"target": {
+					"type": "commit",
+					"message": "auto-test 2",
+					"hash": "19934139a2cf799bbd0f5061ab02e4760902e93f"
+				}
+			}
+		}
+	]
+}
+}`
 )
 
 func Test_detectContentTypeUserAgentAndEventKey(t *testing.T) {
@@ -73,5 +107,299 @@ func Test_detectContentTypeUserAgentAndEventKey(t *testing.T) {
 		require.Equal(t, "", contentType)
 		require.Equal(t, "", userAgent)
 		require.Equal(t, "", eventKey)
+	}
+}
+
+func Test_transformCodePushEvent(t *testing.T) {
+	t.Log("Do Transform - single change")
+	{
+		codePush := CodePushEventModel{
+			PushInfo: PushInfoModel{
+				Changes: []ChangeInfoModel{
+					{
+						ChangeNewItem: ChangeItemModel{
+							Type: "branch",
+							Name: "master",
+							Target: ChangeItemTargetModel{
+								Type:          "commit",
+								CommitHash:    "966d0bfe79b80f97268c2f6bb45e65e79ef09b31",
+								CommitMessage: "auto-test",
+							},
+						},
+					},
+				},
+			},
+		}
+		hookTransformResult := transformCodePushEvent(codePush)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, []bitriseapi.TriggerAPIParamsModel{
+			{
+				CommitHash:    "966d0bfe79b80f97268c2f6bb45e65e79ef09b31",
+				CommitMessage: "auto-test",
+				Branch:        "master",
+			},
+		}, hookTransformResult.TriggerAPIParams)
+	}
+
+	t.Log("Do Transform - multiple changes")
+	{
+		codePush := CodePushEventModel{
+			PushInfo: PushInfoModel{
+				Changes: []ChangeInfoModel{
+					{
+						ChangeNewItem: ChangeItemModel{
+							Type: "branch",
+							Name: "master",
+							Target: ChangeItemTargetModel{
+								Type:          "commit",
+								CommitHash:    "966d0bfe79b80f97268c2f6bb45e65e79ef09b31",
+								CommitMessage: "auto-test",
+							},
+						},
+					},
+					{
+						ChangeNewItem: ChangeItemModel{
+							Type: "branch",
+							Name: "test",
+							Target: ChangeItemTargetModel{
+								Type:          "commit",
+								CommitHash:    "178de4f94efbfa99abede5cf0f1868924222839e",
+								CommitMessage: "auto-test 2",
+							},
+						},
+					},
+				},
+			},
+		}
+		hookTransformResult := transformCodePushEvent(codePush)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, []bitriseapi.TriggerAPIParamsModel{
+			{
+				CommitHash:    "966d0bfe79b80f97268c2f6bb45e65e79ef09b31",
+				CommitMessage: "auto-test",
+				Branch:        "master",
+			},
+			{
+				CommitHash:    "178de4f94efbfa99abede5cf0f1868924222839e",
+				CommitMessage: "auto-test 2",
+				Branch:        "test",
+			},
+		}, hookTransformResult.TriggerAPIParams)
+	}
+
+	t.Log("One of the changes is not a type=branch change")
+	{
+		codePush := CodePushEventModel{
+			PushInfo: PushInfoModel{
+				Changes: []ChangeInfoModel{
+					{
+						ChangeNewItem: ChangeItemModel{
+							Type: "tag",
+							Name: "1.0.0",
+							Target: ChangeItemTargetModel{
+								Type:          "commit",
+								CommitHash:    "966d0bfe79b80f97268c2f6bb45e65e79ef09b31",
+								CommitMessage: "auto-test",
+							},
+						},
+					},
+					{
+						ChangeNewItem: ChangeItemModel{
+							Type: "branch",
+							Name: "test",
+							Target: ChangeItemTargetModel{
+								Type:          "commit",
+								CommitHash:    "178de4f94efbfa99abede5cf0f1868924222839e",
+								CommitMessage: "auto-test 2",
+							},
+						},
+					},
+				},
+			},
+		}
+		hookTransformResult := transformCodePushEvent(codePush)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, []bitriseapi.TriggerAPIParamsModel{
+			{
+				CommitHash:    "178de4f94efbfa99abede5cf0f1868924222839e",
+				CommitMessage: "auto-test 2",
+				Branch:        "test",
+			},
+		}, hookTransformResult.TriggerAPIParams)
+	}
+
+	t.Log("One of the changes is not a type=commit change")
+	{
+		codePush := CodePushEventModel{
+			PushInfo: PushInfoModel{
+				Changes: []ChangeInfoModel{
+					{
+						ChangeNewItem: ChangeItemModel{
+							Type: "branch",
+							Name: "master",
+							Target: ChangeItemTargetModel{
+								Type:          "not-commit",
+								CommitHash:    "966d0bfe79b80f97268c2f6bb45e65e79ef09b31",
+								CommitMessage: "auto-test",
+							},
+						},
+					},
+					{
+						ChangeNewItem: ChangeItemModel{
+							Type: "branch",
+							Name: "test",
+							Target: ChangeItemTargetModel{
+								Type:          "commit",
+								CommitHash:    "178de4f94efbfa99abede5cf0f1868924222839e",
+								CommitMessage: "auto-test 2",
+							},
+						},
+					},
+				},
+			},
+		}
+		hookTransformResult := transformCodePushEvent(codePush)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, []bitriseapi.TriggerAPIParamsModel{
+			{
+				CommitHash:    "178de4f94efbfa99abede5cf0f1868924222839e",
+				CommitMessage: "auto-test 2",
+				Branch:        "test",
+			},
+		}, hookTransformResult.TriggerAPIParams)
+	}
+
+	t.Log("Not a Branch code push event")
+	{
+		codePush := CodePushEventModel{
+			PushInfo: PushInfoModel{
+				Changes: []ChangeInfoModel{
+					{
+						ChangeNewItem: ChangeItemModel{
+							Type: "tag",
+							Name: "1.0.0",
+						},
+					},
+				},
+			},
+		}
+		hookTransformResult := transformCodePushEvent(codePush)
+		require.EqualError(t, hookTransformResult.Error, "'changes' specified in the webhook, but none can be transformed into a build. Collected errors: [Not a type=branch change. Type was: tag]")
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Nil(t, hookTransformResult.TriggerAPIParams)
+	}
+
+	t.Log("Not a 'Commit' type change")
+	{
+		codePush := CodePushEventModel{
+			PushInfo: PushInfoModel{
+				Changes: []ChangeInfoModel{
+					{
+						ChangeNewItem: ChangeItemModel{
+							Type: "branch",
+							Name: "master",
+							Target: ChangeItemTargetModel{
+								Type: "unsupported-type",
+							},
+						},
+					},
+				},
+			},
+		}
+		hookTransformResult := transformCodePushEvent(codePush)
+		require.EqualError(t, hookTransformResult.Error, "'changes' specified in the webhook, but none can be transformed into a build. Collected errors: [Target: Not a type=commit change. Type was: unsupported-type]")
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Nil(t, hookTransformResult.TriggerAPIParams)
+	}
+}
+
+func Test_HookProvider_Transform(t *testing.T) {
+	provider := HookProvider{}
+
+	t.Log("Unsupported Event Type")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"X-Event-Key":  {"not:supported"},
+				"Content-Type": {"application/json"},
+				"User-Agent":   {"Bitbucket-Webhooks/2.0"},
+			},
+		}
+		hookTransformResult := provider.Transform(&request)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "X-Event-Key is not supported: not:supported")
+	}
+
+	t.Log("Unsupported User-Agent")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"X-Event-Key":  {"repo:push"},
+				"Content-Type": {"application/json"},
+				"User-Agent":   {"not/supported"},
+			},
+		}
+		hookTransformResult := provider.Transform(&request)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "User-Agent is not supported: not/supported")
+	}
+
+	t.Log("Unsupported Content-Type")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"X-Event-Key":  {"repo:push"},
+				"Content-Type": {"not/supported"},
+				"User-Agent":   {"Bitbucket-Webhooks/2.0"},
+			},
+		}
+		hookTransformResult := provider.Transform(&request)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "Content-Type is not supported: not/supported")
+	}
+
+	t.Log("No Request Body")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"X-Event-Key":  {"repo:push"},
+				"Content-Type": {"application/json"},
+				"User-Agent":   {"Bitbucket-Webhooks/2.0"},
+			},
+		}
+		hookTransformResult := provider.Transform(&request)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.EqualError(t, hookTransformResult.Error, "Failed to read content of request body: no or empty request body")
+	}
+
+	t.Log("Test with Sample Code Push data")
+	{
+		request := http.Request{
+			Header: http.Header{
+				"X-Event-Key":  {"repo:push"},
+				"Content-Type": {"application/json"},
+				"User-Agent":   {"Bitbucket-Webhooks/2.0"},
+			},
+			Body: ioutil.NopCloser(strings.NewReader(sampleCodePushData)),
+		}
+		hookTransformResult := provider.Transform(&request)
+		require.NoError(t, hookTransformResult.Error)
+		require.False(t, hookTransformResult.ShouldSkip)
+		require.Equal(t, []bitriseapi.TriggerAPIParamsModel{
+			{
+				CommitHash:    "966d0bfe79b80f97268c2f6bb45e65e79ef09b31",
+				CommitMessage: "auto-test",
+				Branch:        "master",
+			},
+			{
+				CommitHash:    "19934139a2cf799bbd0f5061ab02e4760902e93f",
+				CommitMessage: "auto-test 2",
+				Branch:        "test",
+			},
+		}, hookTransformResult.TriggerAPIParams)
 	}
 }
