9940976a4d20882f2047667c3d4d6292f9b00dd3
Viktor Benei <viktor.benei@gmail.com>
2016-02-13 12:51:17 +0100

    Parse the Bitrise Trigger API response if possible

--- bitriseapi/bitriseapi.go
+++ bitriseapi/bitriseapi.go
@@ -25,6 +25,15 @@ type TriggerAPIParamsModel struct {
 	BuildParams BuildParamsModel `json:"build_params"`
 }
 
+// TriggerAPIResponseModel ...
+type TriggerAPIResponseModel struct {
+	Status    string `json:"status"`
+	Message   string `json:"message"`
+	Service   string `json:"service"`
+	AppSlug   string `json:"slug"`
+	BuildSlug string `json:"build_slug"`
+}
+
 // BuildTriggerURL ...
 func BuildTriggerURL(apiRootURL string, appSlug string) (*url.URL, error) {
 	baseURL, err := url.Parse(apiRootURL)
@@ -44,22 +53,25 @@ func BuildTriggerURL(apiRootURL string, appSlug string) (*url.URL, error) {
 //  not a HTTP success response.
 // If the response is an HTTP success response then the whole response body
 //  will be returned, and error will be nil.
-func TriggerBuild(url *url.URL, apiToken string, params TriggerAPIParamsModel, isOnlyLog bool) ([]byte, error) {
+func TriggerBuild(url *url.URL, apiToken string, params TriggerAPIParamsModel, isOnlyLog bool) (TriggerAPIResponseModel, error) {
 	jsonStr, err := json.Marshal(params)
 	if err != nil {
-		return []byte{}, fmt.Errorf("TriggerBuild: failed to json marshal: %s", err)
+		return TriggerAPIResponseModel{}, fmt.Errorf("TriggerBuild: failed to json marshal: %s", err)
 	}
 
 	log.Printf("===> Triggering Build: (url:%s)", url)
 	log.Printf("====> JSON body: %s", jsonStr)
 
 	if isOnlyLog {
-		return []byte("LOG-ONLY-MODE"), nil
+		return TriggerAPIResponseModel{
+			Status:  "ok",
+			Message: "LOG ONLY MODE",
+		}, nil
 	}
 
 	req, err := http.NewRequest("POST", url.String(), bytes.NewBuffer(jsonStr))
 	if err != nil {
-		return []byte{}, fmt.Errorf("TriggerBuild: failed to create request: %s", err)
+		return TriggerAPIResponseModel{}, fmt.Errorf("TriggerBuild: failed to create request: %s", err)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Api-Token", apiToken)
@@ -70,7 +82,7 @@ func TriggerBuild(url *url.URL, apiToken string, params TriggerAPIParamsModel, i
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		return []byte{}, fmt.Errorf("TriggerBuild: failed to send request: %s", err)
+		return TriggerAPIResponseModel{}, fmt.Errorf("TriggerBuild: failed to send request: %s", err)
 	}
 	defer func() {
 		if err := resp.Body.Close(); err != nil {
@@ -80,12 +92,17 @@ func TriggerBuild(url *url.URL, apiToken string, params TriggerAPIParamsModel, i
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return []byte{}, fmt.Errorf("TriggerBuild: request sent, but failed to read response body (http-code:%d): %s", resp.StatusCode, body)
+		return TriggerAPIResponseModel{}, fmt.Errorf("TriggerBuild: request sent, but failed to read response body (http-code:%d): %s", resp.StatusCode, body)
+	}
+
+	var respModel TriggerAPIResponseModel
+	if err := json.Unmarshal(body, &respModel); err != nil {
+		return TriggerAPIResponseModel{}, fmt.Errorf("TriggerBuild: request sent, but failed to parse response (http-code:%d): %s", resp.StatusCode, body)
 	}
 
 	if 200 <= resp.StatusCode && resp.StatusCode <= 202 {
-		return body, nil
+		return respModel, nil
 	}
 
-	return []byte{}, fmt.Errorf("TriggerBuild: request sent, but received a non success response (http-code:%d): %s", resp.StatusCode, body)
+	return respModel, nil
 }
--- service/hook/endpoint.go
+++ service/hook/endpoint.go
@@ -53,13 +53,13 @@ func triggerBuild(triggerURL *url.URL, apiToken string, triggerAPIParams bitrise
 		log.Println(" (debug) isOnlyLog: true")
 	}
 
-	respBody, err := bitriseapi.TriggerBuild(triggerURL, apiToken, triggerAPIParams, isOnlyLog)
+	responseModel, err := bitriseapi.TriggerBuild(triggerURL, apiToken, triggerAPIParams, isOnlyLog)
 	if err != nil {
 		log.Printf(" [!] Exception: failed to trigger build: %s", err)
 		return fmt.Errorf("Failed to Trigger the Build: %s", err)
 	}
 	log.Printf(" ===> trigger build - SUCCESS (%s)", triggerURL)
-	log.Printf("      (debug) response body: (%s)", respBody)
+	log.Printf("      (debug) response: (%#v)", responseModel)
 	return nil
 }
 
