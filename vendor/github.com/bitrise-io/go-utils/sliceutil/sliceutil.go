8e504226f3923d084fa3099bb429cdfe31e88e8a
Viktor Benei <viktor.benei@gmail.com>
2016-10-29 18:09:24 +0200

    deps update (#33)

--- vendor/github.com/bitrise-io/go-utils/sliceutil/sliceutil.go
+++ vendor/github.com/bitrise-io/go-utils/sliceutil/sliceutil.go
@@ -1,5 +1,21 @@
 package sliceutil
 
+// UniqueStringSlice - returns a cleaned up list,
+// where every item is unique.
+// Does NOT guarantee any ordering, the result can
+// be in any order!
+func UniqueStringSlice(strs []string) []string {
+	lookupMap := map[string]interface{}{}
+	for _, aStr := range strs {
+		lookupMap[aStr] = 1
+	}
+	uniqueStrs := []string{}
+	for k := range lookupMap {
+		uniqueStrs = append(uniqueStrs, k)
+	}
+	return uniqueStrs
+}
+
 // IndexOfStringInSlice ...
 func IndexOfStringInSlice(searchFor string, searchIn []string) int {
 	for idx, anItm := range searchIn {
