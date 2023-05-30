9c083c2d3b5223b3290a7ab810d57bc13a935ddb
erosdome <jozsef.eros@bitrise.io>
2023-05-30 09:10:45 +0200

    Add comma

--- service/hook/common/skipci_test.go
+++ service/hook/common/skipci_test.go
@@ -20,7 +20,7 @@ the middle`,
 			`[skip ci] this
 multi
 line too`,
-			`this has \\[skip ci\\] because of markdown`
+			`this has \\[skip ci\\] because of markdown`,
 		} {
 			t.Log(" * Commit message:", aCommitMsg)
 			require.Equal(t, true, IsSkipBuildByCommitMessage(aCommitMsg))
