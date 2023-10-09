7dbff84941fa0e7c1024b90cf02ebca1ce9cc389
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-10-09 09:28:28 +0200

    Handle gitlab push events

--- service/hook/gitlab/metrics.go
+++ service/hook/gitlab/metrics.go
@@ -23,20 +23,65 @@ func (hp HookProvider) GatherMetrics(r *http.Request, appSlug string) (common.Me
 	}
 
 	currentTime := hp.timeProvider.CurrentTime()
-	return hp.gatherMetrics(event, webhookType, appSlug, currentTime), nil
+	return hp.gatherMetrics(event, appSlug, currentTime), nil
 }
 
-func (hp HookProvider) gatherMetrics(event interface{}, webhookType gitlab.EventType, appSlug string, currentTime time.Time) common.Metrics {
+func (hp HookProvider) gatherMetrics(event interface{}, appSlug string, currentTime time.Time) common.Metrics {
 	var metrics common.Metrics
 	switch event := event.(type) {
 	case *gitlab.PushEvent:
-		fmt.Println("action:", event.EventName)
-		metrics = newPushMetrics(event, event.ObjectKind, appSlug)
+		metrics = newPushMetrics(event, appSlug, currentTime)
 	}
 
 	return metrics
 }
 
-func newPushMetrics(event *gitlab.PushEvent, webhookType, appSlug string) *common.PushMetrics {
+func newPushMetrics(event *gitlab.PushEvent, appSlug string, currentTime time.Time) common.PushMetrics {
+	var constructorFunc func(generalMetrics common.GeneralMetrics, commitIDAfter string, commitIDBefore string, oldestCommitTimestamp *time.Time, latestCommitTimestamp *time.Time, masterBranch string) common.PushMetrics
+
+	switch {
+	case isBranchCreate(event):
+		constructorFunc = common.NewPushCreatedMetrics
+	case isBranchDelete(event):
+		constructorFunc = common.NewPushDeletedMetrics
+	default:
+		constructorFunc = common.NewPushMetrics
+	}
+
+	timestamp := (*time.Time)(nil)
+	originalTrigger := fmt.Sprintf("%s:%s", event.EventName, "")
+	userName := event.UserUsername
+	gitRef := event.Ref
+
+	generalMetrics := common.NewGeneralMetrics(currentTime, timestamp, appSlug, originalTrigger, userName, gitRef)
+
+	commitIDAfter := event.After
+	commitIDBefore := event.Before
+	oldestCommitTime := oldestCommitTimestamp(event)
+	latestCommitTime := latestCommitTimestamp(event)
+	masterBranch := event.Project.DefaultBranch
+
+	return constructorFunc(generalMetrics, commitIDAfter, commitIDBefore, oldestCommitTime, latestCommitTime, masterBranch)
+}
+
+func isBranchCreate(event *gitlab.PushEvent) bool {
+	return event.Before == "0000000000000000000000000000000000000000"
+}
+
+func isBranchDelete(event *gitlab.PushEvent) bool {
+	return event.After == "0000000000000000000000000000000000000000"
+}
+
+func oldestCommitTimestamp(event *gitlab.PushEvent) *time.Time {
+	if len(event.Commits) > 0 {
+		return event.Commits[0].Timestamp
+	}
+	return nil
+}
+
+func latestCommitTimestamp(event *gitlab.PushEvent) *time.Time {
+	if len(event.Commits) > 0 {
+		return event.Commits[len(event.Commits)-1].Timestamp
+	}
 	return nil
 }
