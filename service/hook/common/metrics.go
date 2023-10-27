59fed7cb206108e7a2f25f1c3ad2d16ebc31ba55
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-10-27 09:38:40 +0200

    Related prs (#143)
    * Add ProviderType to GeneralMetrics
    
    * Add repo to general metrics
    
    * Add target branch to PR events
    
    * Code cleanup
    
    * Omitempty repository field
    
    * Add comment
    
    * Remove debug logs
    
    * Test push deleted commit is before and after fields
    
    * Fix PushMetrics commit id befor and after json tags
    
    * Fix timezone in commit id before and after tests
    
    * Fix commit id after and before tests
    
    * Fix push event's master branch

--- service/hook/common/metrics.go
+++ service/hook/common/metrics.go
@@ -57,8 +57,8 @@ type PushMetrics struct {
 
 	GeneralMetrics
 
-	CommitIDAfter         string     `json:"commit_id_before,omitempty"`
-	CommitIDBefore        string     `json:"commit_id_after,omitempty"`
+	CommitIDAfter         string     `json:"commit_id_after,omitempty"`
+	CommitIDBefore        string     `json:"commit_id_before,omitempty"`
 	OldestCommitTimestamp *time.Time `json:"oldest_commit_timestamp,omitempty"`
 	LatestCommitTimestamp *time.Time `json:"latest_commit_timestamp,omitempty"`
 	MasterBranch          string     `json:"master_branch,omitempty"`
@@ -150,6 +150,8 @@ func NewPullRequestCommentMetrics(generalMetrics GeneralMetrics, pullRequestID s
 
 // GeneralMetrics ...
 type GeneralMetrics struct {
+	ProviderType    string     `json:"provider_type,omitempty"`
+	Repository      string     `json:"repository,omitempty"` // org/repo
 	TimeStamp       time.Time  `json:"timestamp,omitempty"`
 	EventTimestamp  *time.Time `json:"event_timestamp,omitempty"`
 	AppSlug         string     `json:"app_slug,omitempty"`
@@ -159,8 +161,10 @@ type GeneralMetrics struct {
 }
 
 // NewGeneralMetrics ...
-func NewGeneralMetrics(currentTime time.Time, eventTimestamp *time.Time, appSlug string, originalTrigger string, username string, gitRef string) GeneralMetrics {
+func NewGeneralMetrics(providerType string, repository string, currentTime time.Time, eventTimestamp *time.Time, appSlug string, originalTrigger string, username string, gitRef string) GeneralMetrics {
 	return GeneralMetrics{
+		ProviderType:    providerType,
+		Repository:      repository,
 		TimeStamp:       currentTime,
 		EventTimestamp:  eventTimestamp,
 		AppSlug:         appSlug,
@@ -172,14 +176,17 @@ func NewGeneralMetrics(currentTime time.Time, eventTimestamp *time.Time, appSlug
 
 // GeneralPullRequestMetrics ...
 type GeneralPullRequestMetrics struct {
-	PullRequestID  string `json:"pull_request_id,omitempty"` // PR number
-	CommitID       string `json:"commit_id,omitempty"`
-	ChangedFiles   int    `json:"changed_files_count"`
-	Additions      int    `json:"addition_count"`
-	Deletions      int    `json:"deletion_count"`
-	Commits        int    `json:"commit_count"`
-	MergeCommitSHA string `json:"merge_commit_sha,omitempty"`
-	Status         string `json:"status,omitempty"`
+	PullRequestTitle string `json:"pull_request_title,omitempty"`
+	PullRequestID    string `json:"pull_request_id,omitempty"` // PR number
+	PullRequestURL   string `json:"pull_request_url,omitempty"`
+	TargetBranch     string `json:"target_branch,omitempty"`
+	CommitID         string `json:"commit_id,omitempty"`
+	ChangedFiles     int    `json:"changed_files_count"`
+	Additions        int    `json:"addition_count"`
+	Deletions        int    `json:"deletion_count"`
+	Commits          int    `json:"commit_count"`
+	MergeCommitSHA   string `json:"merge_commit_sha,omitempty"`
+	Status           string `json:"status,omitempty"`
 }
 
 // Serialise ...
--- service/hook/github/metrics_test.go
+++ service/hook/github/metrics_test.go
@@ -1,14 +1,44 @@
 package github
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 
+	"github.com/bitrise-io/bitrise-webhooks/service/hook/common"
 	"github.com/google/go-github/v55/github"
 	"github.com/stretchr/testify/require"
-
-	"github.com/bitrise-io/bitrise-webhooks/service/hook/common"
 )
+
+func TestHookProvider_gatherMetrics_commit_id_before_and_after(t *testing.T) {
+	currentTime := time.Date(2023, time.October, 26, 8, 0, 0, 0, time.UTC)
+
+	tests := []struct {
+		name        string
+		event       interface{}
+		webhookType string
+		appSlug     string
+		want        string
+	}{
+		{
+			name:        "Push deleted webhook - commit id after is null, before isn't",
+			event:       testPushWebhook(t),
+			webhookType: "git-push",
+			appSlug:     "slug",
+			want:        `{"event":"git_push","action":"deleted","provider_type":"github","repository":"bitrise-io/project","timestamp":"2023-10-26T08:00:00Z","app_slug":"slug","original_trigger":"git-push:","user_name":"bitrise-bot","git_ref":"refs/heads/tech_improvements","commit_id_after":"0000000000000000000000000000000000000000","commit_id_before":"123ddfe9f740fb229b9cff3e43a484bbcedf7fa8","master_branch":"main"}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hp := HookProvider{}
+			got := hp.gatherMetrics(tt.event, tt.webhookType, tt.appSlug, currentTime)
+			gotBytes, err := got.Serialise()
+			require.NoError(t, err)
+			require.Equal(t, tt.want, string(gotBytes))
+		})
+	}
+
+}
 
 func TestHookProvider_gatherMetrics(t *testing.T) {
 	currentTime := time.Now()
@@ -29,6 +59,7 @@ func TestHookProvider_gatherMetrics(t *testing.T) {
 				Event:  "git_push",
 				Action: "pushed",
 				GeneralMetrics: common.GeneralMetrics{
+					ProviderType:    ProviderID,
 					TimeStamp:       currentTime,
 					AppSlug:         "slug",
 					OriginalTrigger: "git-push:",
@@ -44,6 +75,7 @@ func TestHookProvider_gatherMetrics(t *testing.T) {
 				Event:  "pull_request",
 				Action: "updated",
 				GeneralMetrics: common.GeneralMetrics{
+					ProviderType:    ProviderID,
 					TimeStamp:       currentTime,
 					AppSlug:         "slug",
 					OriginalTrigger: "pull_request:",
@@ -62,6 +94,7 @@ func TestHookProvider_gatherMetrics(t *testing.T) {
 				Event:  "pull_request",
 				Action: "comment",
 				GeneralMetrics: common.GeneralMetrics{
+					ProviderType:    ProviderID,
 					TimeStamp:       currentTime,
 					AppSlug:         "slug",
 					OriginalTrigger: "pull_request_review_comment:",
@@ -85,3 +118,37 @@ func TestHookProvider_gatherMetrics(t *testing.T) {
 		})
 	}
 }
+
+func testPushWebhook(t *testing.T) interface{} {
+	var event github.PushEvent
+	err := json.Unmarshal([]byte(pushDeletedWebhookPayload), &event)
+	require.NoError(t, err)
+	return &event
+}
+
+const pushDeletedWebhookPayload = `{
+  "ref": "refs/heads/tech_improvements",
+  "before": "123ddfe9f740fb229b9cff3e43a484bbcedf7fa8",
+  "after": "0000000000000000000000000000000000000000",
+  "repository": {
+    "full_name": "bitrise-io/project",
+    "html_url": "https://github.com/bitrise-io/project",
+    "default_branch": "main",
+    "master_branch": "main"
+  },
+  "pusher": {
+    "name": "bitrise-bot",
+    "email": "bitrise-bote@users.noreply.github.com"
+  },
+  "sender": {
+    "login": "bitrise-bot"
+  },
+  "created": false,
+  "deleted": true,
+  "forced": false,
+  "base_ref": null,
+  "commits": [
+
+  ],
+  "head_commit": null
+}`
