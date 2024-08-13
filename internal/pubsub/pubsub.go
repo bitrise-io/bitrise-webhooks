fd0142cba226b29a3859a757fe2a252ecae7d520
miklos.boros <miklos.boros@bitrise.io>
2024-08-13 10:43:38 +0200

    [SSW-1713] Removes possibly hanging network wait on metrics

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -4,9 +4,10 @@ import (
 	"context"
 
 	"cloud.google.com/go/pubsub"
-	"github.com/bitrise-io/bitrise-webhooks/service/hook/common"
 	"github.com/pkg/errors"
 	"google.golang.org/api/option"
+
+	"github.com/bitrise-io/bitrise-webhooks/service/hook/common"
 )
 
 // Client ...
@@ -40,11 +41,7 @@ func (c *Client) PublishMetrics(ctx context.Context, metrics common.Metrics) (er
 
 	msg := pubsub.Message{Data: b}
 
-	topic := c.pubsubClient.Topic(c.pubsubTopicID)
-	result := topic.Publish(ctx, &msg)
-	serverID, err := result.Get(ctx)
-	if err != nil {
-		return errors.Wrap(errors.WithStack(err), "serverID: "+serverID)
-	}
+	_ = c.pubsubClient.Topic(c.pubsubTopicID).Publish(ctx, &msg)
+
 	return nil
 }
