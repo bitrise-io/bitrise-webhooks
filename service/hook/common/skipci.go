d6161484cc26eca0063600380d11bfc495c64027
Tamas Papik <trapacska@gmail.com>
2025-05-14 14:55:27 +0200

    added bitrise skip

--- service/hook/common/skipci.go
+++ service/hook/common/skipci.go
@@ -4,12 +4,22 @@ import "strings"
 
 // IsSkipBuildByCommitMessage ...
 func IsSkipBuildByCommitMessage(commitMsg string) bool {
-	if strings.Contains(commitMsg, "[skip ci]") ||
-		strings.Contains(commitMsg, "[ci skip]") ||
-		strings.Contains(commitMsg, `\[skip ci\]`) ||
-		strings.Contains(commitMsg, `\[ci skip\]`) ||
-		strings.Contains(commitMsg, `\\[skip ci\\]`) ||
-		strings.Contains(commitMsg, `\\[ci skip\\]`) {
+	if checkSkipPatternPair(commitMsg, "ci", "skip") {
+		return true
+	}
+	if checkSkipPatternPair(commitMsg, "bitrise", "skip") {
+		return true
+	}
+	return false
+}
+
+func checkSkipPatternPair(commitMsg, a, b string) bool {
+	if strings.Contains(commitMsg, "["+a+" "+b+"]") ||
+		strings.Contains(commitMsg, "["+b+" "+a+"]") ||
+		strings.Contains(commitMsg, `\[`+a+` `+b+`\]`) ||
+		strings.Contains(commitMsg, `\[`+b+` `+a+`\]`) ||
+		strings.Contains(commitMsg, `\\[`+a+` `+b+`\\]`) ||
+		strings.Contains(commitMsg, `\\[`+b+` `+a+`\\]`) {
 		return true
 	}
 	return false
