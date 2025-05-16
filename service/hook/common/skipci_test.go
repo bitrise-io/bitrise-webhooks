4298def9c79f49afd0f4af492a32e74702978320
Tamas Papik <trapacska@gmail.com>
2025-05-16 11:17:12 +0200

    added bitrise skip (#224)

--- service/hook/common/skipci_test.go
+++ service/hook/common/skipci_test.go
@@ -6,6 +6,43 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestIsSkipBuildByCommitMessage_Bitrise(t *testing.T) {
+	t.Log("Should skip")
+	{
+		for _, aCommitMsg := range []string{
+			"[bitrise skip]",
+			"[skip bitrise]",
+			`multi line
+with [bitrise skip] in
+the middle`,
+			"this should be [bitrise skip]ped",
+			"[skip bitrise] will skip",
+			`[skip bitrise] this
+multi
+line too`,
+			`this message has \[skip bitrise\] because of markdown`,
+			`this message has \\[skip bitrise\\] because of markdown`,
+		} {
+			t.Log(" * Commit message:", aCommitMsg)
+			require.Equal(t, true, IsSkipBuildByCommitMessage(aCommitMsg))
+		}
+	}
+
+	t.Log("Should NOT skip")
+	{
+		for _, aCommitMsg := range []string{
+			"",
+			"[BITRISE SKIP]",
+			"[SKIP BITRISE]",
+			"this will not be [BITRISE SKIP]ped",
+			"this won't be skipped either: [ bitrise skip ]",
+		} {
+			t.Log(" * Commit message:", aCommitMsg)
+			require.Equal(t, false, IsSkipBuildByCommitMessage(aCommitMsg))
+		}
+	}
+}
+
 func TestIsSkipBuildByCommitMessage(t *testing.T) {
 	t.Log("Should skip")
 	{
